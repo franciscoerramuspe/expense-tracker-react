8d7b81b604ef18a3d0a54a14b5e6f5c6445ffc60
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2023-03-06 17:02:03 -0600

    added expense label

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -22,6 +22,46 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
             {/* class name is formulario, and if animarModal is true, add animar as className, else add cerrar*/}
             <form className={`formulario ${animarModal ? "animar" : 'cerrar' }`}>
                 <legend>New Expense</legend>
+                <div className='campo'>
+                    <label htmlFor="nombre">Expense Name</label>
+
+                    <input 
+                        type="text"
+                        placeholder='Add the name of the expense'
+                        id="nombre"
+                    />
+                </div>
+                <div className='campo'>
+                    <label htmlFor="cantidad">Quantity</label>
+
+                    <input 
+                        type="number"
+                        placeholder='Add the amount of the expense'
+                        id="cantidad"
+                    />
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor="categoria">Category</label>
+
+                    <select id="categoria">
+                        <option value="">-- Select --</option>
+                        <option value="ahorro">Savings</option>
+                        <option value="comida">Food</option>
+                        <option value="casa">Housing</option>
+                        <option value="ocio">Leisure</option>
+                        <option value="salud">Health</option>
+                        <option value="suscripciones">Suscriptions</option>
+                        <option value="gastos">Others</option>
+
+
+
+                    </select>
+                </div>
+            
+                <input type="submit" 
+                    value="Add expense"
+                />
             </form>
         </div>
   )
