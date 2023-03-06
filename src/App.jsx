759ea43741beb046c5f6f4befb4c0ce1ab1a61b5
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2023-03-05 21:53:12 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import IconNewExpense from './img/nuevo-gasto.svg'
 
 
 function App() {
   
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidBudget, setIsValidBudget] = useState(false)
+
+  const handleNewBudget = () => {
+    console.log('hol')
+  }
   return (
     <div>
       <Header
@@ -15,6 +20,17 @@ function App() {
         setIsValidBudget = {setIsValidBudget}
 
       />
+
+      {isValidBudget && (
+        <div className='nuevo-gasto'>
+          <img 
+            src={IconNewExpense}
+            alt='Icon new expense'
+            onClick={handleNewBudget}
+            />
+      </div>
+      )}
+      
     </div>
   )
 }
