dc8346523fc6e093f573efd5e75b20d363633e9d
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2023-03-10 23:17:36 -0600

    added expense icons

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import { formatDate } from '../helpers';
+import SavingsIcon from '../img/icono_ahorro.svg'
+import HouseIcon from '../img/icono_casa.svg'
+import ExpenseIcon from '../img/icono_gastos.svg'
+import FoodIcon from '../img/icono_comida.svg'
+import LeisureIcon from '../img/icono_ocio.svg'
+import HealthIcon from '../img/icono_salud.svg'
+import SubscriptionsIcon from '../img/icono_suscripciones.svg'
+
+const iconDictionary = {
+  Savings : SavingsIcon,
+  Food : FoodIcon,
+  Housing : HouseIcon,
+  Leisure : LeisureIcon,
+  Health : HealthIcon,
+  Suscriptions : SubscriptionsIcon,
+  Others : ExpenseIcon
+}
 
 const Expense = ({expense}) => {
   const { category, name, price, id, date } = expense;
   return (
     <div className='gasto sombra'>
         <div className='contenido-gasto'>
+          <img
+            src={iconDictionary[category]}
+            alt="Expense Icon" 
+
+          />
             <div className='descripcion-gasto'>
                 <p className='categoria'>{category}</p>
                 <p className="nombre-gasto">{name}</p>
