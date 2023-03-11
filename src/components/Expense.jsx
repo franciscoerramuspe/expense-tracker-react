a0e8405c6ee580cbacfe6f27b0fec3924d53546b
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2023-03-10 22:56:31 -0600

    added date fromatting

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import { formatDate } from '../helpers';
 
 const Expense = ({expense}) => {
-  const { category, name, price, id } = expense;
+  const { category, name, price, id, date } = expense;
   return (
     <div className='gasto sombra'>
         <div className='contenido-gasto'>
             <div className='descripcion-gasto'>
                 <p className='categoria'>{category}</p>
                 <p className="nombre-gasto">{name}</p>
+                <p className="fecha-gasto">
+                  Added in: {''}
+                  <span>{formatDate(date)}</span>
+                </p>
             </div>
         </div>
+        <p className="cantidad-gasto">${price}</p>
     </div>
   )
 }
